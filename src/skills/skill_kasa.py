c606f913878d483d977e13fdfa19f8adb31432e4
alexh-git2 <alexkfhui@gmail.com>
2026-04-01 18:31:04 -0700

    fix skill_kasa execte function

--- src/skills/skill_kasa.py
+++ src/skills/skill_kasa.py
@@ -21,25 +21,34 @@ SKILL = {
       * "Turn off the espresso machine"
       * "Turn on the coffee bar lights"
       * "Turn off the coffee bar lights    
-    Example: {{"function": "kasa", "parameters": {{"prompt": "Turn on my coffee machine"}}}}""",
+    Example: {{"function": "kasa", "parameters": {{"device_name": "coffee_machine", "action": "off"}}}}""",
     "examples": [
         """Example - Turn on coffee or espresso machine:
 User: "Turn on my coffee machine"
-Response: {{"reply": "Turning on coffee machine", "function_calls": [{{"function": "kasa", "parameters": {{"kitchen": "espresso machine", "action": "coffee_machine"}}}}], "new_memories": []}}""",
+Response: {{"reply": "Turning on coffee machine", "function_calls": [{{"function": "kasa", "parameters": {{"device_name": "coffee_machine", "action": "on"}}}}], "new_memories": []}}""",
         """Example - Turn on or off the coffee bar lights:
 User: "Turn on my coffee bar lights"
-Response: {{"reply": "Turning on coffee bar lights", "function_calls": [{{"function": "kasa", "parameters": {{"kitchen": "coffee bar lights", "action": "coffee_bar_lights"}}}}], "new_memories": []}}""",
+Response: {{"reply": "Turning on coffee bar lights", "function_calls": [{{"function": "kasa", "parameters": {{"device_name": "coffee_bar_lights", "action": "on"}}}}], "new_memories": []}}""",
     ],
 }
 
 
 def execute(parameters, context):
     from modules.module_messageQue import queue_message
+    from modules.module_kasa import turn_on, turn_off
 
     skill_config = context.get("skill_config", {})
 
     queue_message(f"parameters: {parameters}")
 
-    device_name = skill_config.get("coffee_bar_lights", {})
-    queue_message(f"devname={device_name}")
-    return "I couldn't get a response from Kasa."
+    device_name = skill_config.get(parameters.device_name, {})
+    if parameters.action == "on":
+        turn_on(device_name)
+        queue_message(f"Turning on {device_name}")
+        return f"Turning on {device_name}"
+    elif parameters.action == "off":
+        turn_off(device_name)
+        queue_message(f"Turning off {device_name}")
+        return f"Turning off {device_name}"
+
+    return "I didn't understand your request."
